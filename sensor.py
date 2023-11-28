fc9e3fd2bdb5358d0bdf37fb47616d0e06d8d43a
Neil Bezuidenhout <34944157+TheOtherNeo@users.noreply.github.com>
2023-11-28 16:30:22 +0400

    Update sensor.py
    @sashyazs Base values from documentation only for testing.

--- sensor.py
+++ sensor.py
@@ -1,20 +1,44 @@
-#To check distance (if obstruction is within range of 10 cm) and adjust lights if so
-# the hand must remain for 5 seconds to switch off the light
+# To check distance (if obstruction is within range of 10 cm) and adjust lights if so
+# the hand must remain for 5 seconds to switch off the light*/
 
-from gpiozero import DistanceSensor
-from gpiozero.pins.pigpio import PiGPIOFactory
+# See this document for futher information https://gpiozero.readthedocs.io/en/latest/recipes.html#distance-sensor
+
+# Check that the pin values match the values specified for the DistanceSensor
+
+from gpiozero import DistanceSensor, LED
 from signal import pause
+from time import sleep
 
-pi_gpio_factory = PiGPIOFactory()
+def measure_distance():
 
-sensor = DistanceSensor(echo=17, trigger=4, max_distance=2, threshold_distance=0.05, pin_factory=pi_gpio_factory)
+    sensor = DistanceSensor(23, 24)
 
-while True:
-    sensor.wait_for_in_range()
-    print('In range') 
-    sensor.wait_for_out_of_range()
-    print('Out of range')
+    while True:
+        print('Distance to nearest object is', sensor.distance, 'm')
+        sleep(1)
 
-pause()
+def take_aciton():
+    sensor = DistanceSensor(23, 24, max_distance=1, threshold_distance=0.2)
+    led = LED(16)
 
+    sensor.when_in_range = led.on
+    sensor.when_out_of_range = led.off
 
+    pause()
+
+    
+def main():
+    print("1: Measure Distance")
+    print("2: Perform an action")
+    action = input("What would you like to do? ")
+
+    match action:
+        case "1":
+            print("Measuring Distance...")
+            measure_distance
+        case "2":
+            print("Taking aciton on Distance...")
+            take_aciton
+
+if __name__ == "__main__":
+    main()
